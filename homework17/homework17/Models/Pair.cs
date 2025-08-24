3e5f90b0b0f2a083770a0556403519f1a1decbee
AytajMammadova306 <aytajamam@code.edu.az>
2025-08-24 11:03:50 +0400

    initial

---
+++ homework17/homework17/Models/Pair.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace homework17.Models
+{
+    internal class Pair<T, U> 
+    {
+        public T Key {  get; set; }
+        public U Value { get; set; }
+        public static Pair<T, U>[] Pairs = new Pair<T, U>[0];
+        public U this[ T index]
+        {
+            get
+            {
+                foreach (Pair<T, U> pair in Pairs)
+                {
+                    if (pair.Key.Equals(index))
+                        return pair.Value;
+                }
+                throw new Exception("wrong input");
+            }
+            set
+            {
+                foreach (Pair<T, U> pair in Pairs)
+                {
+                    if (pair.Key.Equals(index))
+                    {
+                        pair.Value = value;
+                        return;
+                    }
+                }
+                Console.WriteLine("value not found");
+
+            }
+        }
+        public static void Add (Pair<T,U> pair)
+        {
+            int count = 0;
+            foreach (Pair<T,U>  p in Pairs)
+            {
+                if (p.Key.Equals(pair.Key))
+                    count++;
+            }
+            if (count == 0)
+            {
+                Array.Resize(ref Pairs, Pairs.Length+1);
+                Pairs[Pairs.Length-1] = pair;
+            }
+            else Console.WriteLine("this key was already used");
+        }
+        public static void PrintInfo()
+        {
+            foreach (Pair<T,U> p in Pairs)
+            {
+                Console.WriteLine($"Key:{p.Key}, Value:{p.Value}");
+            }
+        }
+    }
+}
---
+++ homework17/homework17/Program.cs
@@ -0,0 +1,48 @@
+﻿using homework17.Models;
+
+namespace homework17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pair<int, string> pair = new Pair<int, string>
+            {
+                Key=1,
+                Value="samebody"
+            };
+            Pair<int, int> pair2 = new Pair<int, int>
+            {
+                Key = 2,
+                Value = 102
+            };
+            Pair<int, int> pair3 = new Pair<int, int>
+            {
+                Key = 3,
+                Value = 103
+            };
+            Pair<int, string>.Add(pair);
+            Pair<int, int>.Add(pair2);
+            Pair<int, int>.Add(pair3);
+            Pair<int,string>.PrintInfo();
+            Pair<int, int>.PrintInfo();
+
+            //Console.WriteLine(pair2[1]);
+            //exception verecey hal
+
+            Console.WriteLine("------------------");
+            Pair<string, int> pair4 = new Pair<string, int>
+            {
+                Key = "dorduncu",
+                Value = 104
+            }; Pair<string, int> pair5 = new Pair<string, int>
+            {
+                Key = "besinci",
+                Value = 105
+            };
+            Pair<string, int>.Add(pair4);
+            Pair<string, int>.Add(pair5);
+            Pair<string,int>.PrintInfo();
+        }
+    }
+}
